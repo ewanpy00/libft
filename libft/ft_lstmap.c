57795de2970e759f9839eddc4f96dac41ae43749
Ivan <ewanpyh@gmail.com>
2025-11-06 13:36:13 +0400

    Changes

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ivan <ivan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 10:58:03 by ivan              #+#    #+#             */
-/*   Updated: 2025/11/06 13:12:16 by ivan             ###   ########.fr       */
+/*   Updated: 2025/11/06 13:35:26 by ivan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*new_node;
+	t_list	*new_list;
+	t_list	*new_elem;
+	void	*new_content;
 
-	if (!lst || !f)
+	if (!f || !del)
 		return (NULL);
-	new_lst = NULL;
-	new_node = NULL;
+	new_list = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		new_content = f(lst->content);
+		new_elem = ft_lstnew(new_content);
+		if (!new_elem)
 		{
-			del(new_node);
-			ft_lstclear(&new_lst, del);
+			del(new_content);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_lst, new_node);
+		ft_lstadd_back(&new_list, new_elem);
 		lst = lst->next;
 	}
-	return (new_lst);
+	return (new_list);
 }
